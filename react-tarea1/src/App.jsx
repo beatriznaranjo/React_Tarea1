a51a9cc5d59a065d82f6f49166504d2b7f357ef7
Beatriz Naranjo <beanaranjo@gmail.com>
2022-07-28 09:52:46 -0600

    proyecto listo

--- react-tarea1/src/App.jsx
+++ react-tarea1/src/App.jsx
@@ -21,15 +21,15 @@ export default function App() {
   return (
     <div className="container">
       <h1>Tarea 1 - React</h1>
-      <h2>Ejercicio 1</h2>
+      <h2>EJERCICIO 1</h2>
       <p>Componente que recibe un prop llamado cargando: si cargando es verdadero entonces el texto del button debe decir
 "cargando" y si no debe decir "listo" </p>
       <ButtonCarga cargando={carga} cambiarCarga={cambiarCarga}/>
-      <h2>Ejercicio 2</h2>
+      <h2>EJERCICIO 2</h2>
       <p>Cree un componente que tenga un array de animales y usando la funcion .map() me
 retorne un párrafo para cada animal del array</p>
       <Animales />
-      <h2>Ejercicio 3</h2>
+      <h2>EJERCICIO 3</h2>
       <p>Copiar la funcionalidad incluida en <a href="https://react-zu9llg.stackblitz.io/">https://react-zu9llg.stackblitz.io/</a></p>
       <Count currentCount={count} incrementar={incrementar}/>
     </div>
